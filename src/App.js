84e89d9c1bb7ed1c21afa5cc77b637d68ea28390
liqin.pan <liqin.pan@xuncetech.com>
2021-07-27 10:55:20 +0800

    feat 增加文章列表

--- src/App.js
+++ src/App.js
@@ -3,31 +3,35 @@ import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { adminRouter } from './routes'
 import { Frame } from './components'
+import zhCN from 'antd/lib/locale/zh_CN';
+import { ConfigProvider } from 'antd'
 
 const menus = adminRouter.filter(item => item.isNav)
 
 function App() {
   return (
-    <Frame menus={menus}>
-      <Switch>
-        {
-          adminRouter.map(route => { // 页面级别的权限路由
-            return (
-              <Route
-                exact
-                key={route.pathname}
-                path={route.pathname}
-                render={(routeProps) => {
-                  return <route.component {...routeProps} />
-                }}
-              />
-            )
-          })
-        }
-        <Redirect to={adminRouter[0].pathname} from='/admin' exact />
-        <Redirect to='/404' />
-      </Switch>
-    </Frame>
+    <ConfigProvider locale={zhCN}>
+      <Frame menus={menus}>
+        <Switch>
+          {
+            adminRouter.map(route => { // 页面级别的权限路由
+              return (
+                <Route
+                  exact
+                  key={route.pathname}
+                  path={route.pathname}
+                  render={(routeProps) => {
+                    return <route.component {...routeProps} />
+                  }}
+                />
+              )
+            })
+          }
+          <Redirect to={adminRouter[0].pathname} from='/admin' exact />
+          <Redirect to='/404' />
+        </Switch>
+      </Frame>      
+    </ConfigProvider>
   )
 }
 
